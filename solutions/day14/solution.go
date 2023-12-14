1b92d384d39f532765a0b37ca2604e1c369829f3
Vedika Chandra <94168243+vcvedika@users.noreply.github.com>
2023-12-14 22:26:59 +0530

    Day 14 (#12)

--- solutions/day14/solution.go
+++ solutions/day14/solution.go
@@ -2,44 +2,111 @@ package main
 
 import "fmt"
 
-func NorthBeams(arr [][]rune) int {
+func rollNorth(arr [][]rune) {
+
+	for i := 0; i < len(arr[0]); i++ {
+		for j := 0; j < len(arr); j++ {
+			if arr[j][i] == 'O' {
+				pos := j
+				for k := j - 1; k >= 0; k-- {
+					if arr[k][i] == '.' {
+						pos = k
+					} else {
+						break
+					}
+				}
+				arr[pos][i], arr[j][i] = arr[j][i], arr[pos][i]
+			}
+		}
+	}
+}
+
+func rollSouth(arr [][]rune) {
+
+	for i := 0; i < len(arr[0]); i++ {
+		for j := len(arr) - 1; j >= 0; j-- {
+			if arr[j][i] == 'O' {
+				pos := j
+				for k := j + 1; k < len(arr); k++ {
+					if arr[k][i] == '.' {
+						pos = k
+					} else {
+						break
+					}
+				}
+				arr[pos][i], arr[j][i] = arr[j][i], arr[pos][i]
+			}
+		}
+	}
+}
+
+func rollWest(arr [][]rune) {
+
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr[0]); j++ {
+			if arr[i][j] == 'O' {
+				pos := j
+				for k := j - 1; k >= 0; k-- {
+					if arr[i][k] == '.' {
+						pos = k
+					} else {
+						break
+					}
+				}
+				arr[i][pos], arr[i][j] = arr[i][j], arr[i][pos]
+			}
+		}
+	}
+}
+
+func rollEast(arr [][]rune) {
+
+	for i := 0; i < len(arr); i++ {
+		for j := len(arr[0]) - 1; j >= 0; j-- {
+			if arr[i][j] == 'O' {
+				pos := j
+				for k := j + 1; k < len(arr[0]); k++ {
+					if arr[i][k] == '.' {
+						pos = k
+					} else {
+						break
+					}
+				}
+				arr[i][pos], arr[i][j] = arr[i][j], arr[i][pos]
+			}
+		}
+	}
+}
+
+func solve(arr [][]rune) int {
 
 	ans := 0
-	row := len(arr)
-	col := len(arr[0])
-	mp := make(map[int]int)
-
-	for i := 0; i < col; i++ {
-		var seq []int
-		var cnt []int
-		str := "#"
-		temp := 0
-		for j := 0; j < row; j++ {
-			str += string(rune(arr[j][i]))
-		}
-		str += "#"
-		for in, elem := range str {
-			if rune(elem) == 'O' {
-				temp++
-			} else if rune(elem) == '#' {
-				seq = append(seq, in)
-				cnt = append(cnt, temp)
-				temp = 0
-			}
-		}
-		for j := 1; j < len(seq); j++ {
-			for k := 0; k < cnt[j]; k++ {
-				mp[seq[j-1]+k]++
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr[0]); j++ {
+			if arr[i][j] == 'O' {
+				ans += len(arr) - i
 			}
 		}
 	}
-
-	for i := 0; i < row; i++ {
-		ans += mp[i] * col
-		col--
-	}
-
 	return ans
+}
+
+func convert(arr [][]rune) string {
+
+	ans := ""
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < len(arr[0]); j++ {
+			ans += string(rune(arr[i][j]))
+		}
+	}
+	return ans
+}
+
+func cycle(arr [][]rune) {
+	rollNorth(arr)
+	rollWest(arr)
+	rollSouth(arr)
+	rollEast(arr)
 }
 
 func part1() int {
@@ -56,9 +123,42 @@ func part1() int {
 		arr = append(arr, []rune(input))
 	}
 
-	return NorthBeams(arr)
+	rollNorth(arr)
+	return solve(arr)
+}
+
+func part2() int {
+
+	var arr [][]rune
+
+	for {
+		var input string
+		fmt.Scanln(&input)
+
+		if input == "" {
+			break
+		}
+		arr = append(arr, []rune(input))
+	}
+
+	mp := make(map[string]int)
+
+	for i := 1; i <= 1000000000; i++ {
+		cycle(arr)
+		if elem, ok := mp[convert(arr)]; ok {
+			rem := (1000000000 - elem) % (i - elem)
+			for j := 0; j < rem; j++ {
+				cycle(arr)
+			}
+			return solve(arr)
+		}
+		mp[convert(arr)] = i
+
+	}
+	return 1
 }
 
 func main() {
 	fmt.Println(part1())
+	// fmt.Println(part2())
 }
